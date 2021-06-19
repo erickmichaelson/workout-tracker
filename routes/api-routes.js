d243130ade6c9ae5adb7c274db99e4f9b15ddd27
Erick Michaelson <emich09@gmail.com>
2021-06-19 17:20:49 -0400

    fixed excercises object data in workout.js

---
+++ routes/api-routes.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const db = require("../models/Workout");
+
+//route to get workouts
+router.get("/api/workouts", (req, res) => {
+    db.find({})
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+//route to create new workouts
+router.post("/api/workouts", (req, res) => {
+    db.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+//route to add exercises
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    db.findByIdAndUpdate(params.id, { $push: { exercises: body } }, { new: true, runValidators: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+//route for workouts in range
+router.get("/api/workouts/range", (req, res) => {
+    db.aggregate([{
+        $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration"
+            }
+        }
+    }])
+        .sort({ _id: -1 })
+        .limit(7)
+        .then(dbWorkout => {
+
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
